2971336a19815b071a665fd7f81aec09933639b4
Amr Yasser <154547867+AMRYB@users.noreply.github.com>
2024-04-28 20:09:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function toggleVisibility(id) {
+    var element = document.getElementById(id);
+    if (element.style.display === 'block') {
+        element.style.display = 'none';
+    } else {
+        element.style.display = 'block';
+    }
+}
+
+document.getElementById('date').textContent = new Date().toLocaleDateString();
